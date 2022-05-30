d7b61934b685b853d735309d0cc95473ff392ed6
claudiofilgueira <claudiofilgueira@users.noreply.github.com>
2022-05-30 18:29:03 -0300

    Add files via upload

--- cadastroAluno/gravaAluno.php
+++ cadastroAluno/gravaAluno.php
@@ -8,25 +8,26 @@ require_once("connect.php");
 global $conn;
 
 if ($_POST){
-    $cpf = (isset($_POST['cpf'])) ? $cpf = $_POST['cpf'] : mysqli_close($conn);
-    $nome = (isset($_POST['nome'])) ? $nome = $_POST['nome'] : mysqli_close($conn);
-    $email = (isset($_POST['email'])) ? $email = $_POST['email'] :mysqli_close($conn);
-    $telefone = (isset($_POST['telefone'])) ? $telefone = $_POST['telefone'] :mysqli_close($conn);
-    $nascimento = (isset($_POST['nascimento'])) ? $nascimento = $_POST['nascimento'] :mysqli_close($conn);
-    $endereco = (isset($_POST['endereco'])) ? $endereco = $_POST['endereco'] :mysqli_close($conn);
-    $bairro = (isset($_POST['bairro'])) ? $bairro = $_POST['bairro'] :mysqli_close($conn);
-    $escolaridade = (isset($_POST['escolaridade'])) ? $escolaridade = $_POST['escolaridade'] :mysqli_close($conn);
-    $estuda = (isset($_POST['estuda'])) ? $estuda = $_POST['estuda'] :mysqli_close($conn);
-    $instituicao = (isset($_POST['instituicao'])) ? $instituicao = $_POST['instituicao'] :mysqli_close($conn);
-    $curso = (isset($_POST['curso'])) ? $curso = $_POST['curso'] :mysqli_close($conn);
-    $motivo = (isset($_POST['motivo'])) ? $motivo = $_POST['motivo'] :mysqli_close($conn);
-    $razao = (isset($_POST['razao'])) ? $razao = $_POST['razao'] :mysqli_close($conn);
-    $experiencia = (isset($_POST['experiencia'])) ? $experiencia = $_POST['experiencia'] :mysqli_close($conn);
-    $expectativa = (isset($_POST['expectativa'])) ? $endereco = $_POST['expectativa'] :mysqli_close($conn);
-    $urlFoto = (isset($_POST['urlFoto'])) ? $urlFoto = $_POST['urlFoto'] :mysqli_close($conn);
+    $cpf = (isset($_POST['cpf'])) ? $cpf = $_POST['cpf'] : "";
+    $nome = (isset($_POST['nome'])) ? $nome = $_POST['nome'] :"";
+    $email = (isset($_POST['email'])) ? $email = $_POST['email'] :"";
+    $telefone = (isset($_POST['telefone'])) ? $telefone = $_POST['telefone'] :"";
+    $nascimento = (isset($_POST['nascimento'])) ? $nascimento = $_POST['nascimento'] :"";
+    $endereco = (isset($_POST['endereco'])) ? $endereco = $_POST['endereco'] :"";
+    $bairro = (isset($_POST['bairro'])) ? $bairro = $_POST['bairro'] :"";
+    $escolaridade = (isset($_POST['escolaridade'])) ? $escolaridade = $_POST['escolaridade'] :"";
+    $estuda = (isset($_POST['estuda'])) ? $estuda = $_POST['estuda'] :"";
+    $instituicao = (isset($_POST['instituicao'])) ? $instituicao = $_POST['instituicao'] :"";
+    $curso = (isset($_POST['curso'])) ? $curso = $_POST['curso'] :"";
+    $motivo = (isset($_POST['motivo'])) ? $motivo = $_POST['motivo'] :"";
+    $razao = (isset($_POST['razao'])) ? $razao = $_POST['razao'] :"";
+    $experiencia = (isset($_POST['experiencia'])) ? $experiencia = $_POST['experiencia'] :"";
+    $expectativa = (isset($_POST['expectativa'])) ? $expectativa = $_POST['expectativa'] :"";
+    $urlFoto = (isset($_POST['urlFoto'])) ? $urlFoto = $_POST['urlFoto'] :"";
 
+    
 echo "CPF".$cpf."<br>", "nome".$nome."<br>","e-mail".$email."<br>", "Telefone".$telefone."<br>", "Nascimento" .$nascimento."<br>","endereço: ".$endereco."<br>", "bairro".$bairro."<br>";
-echo $escolaridade."<br>", $estuda."<br>", $instituicao."<br>", $curso."<br>", $motivo."<br>", $razao."<br>", $experiencia."<br>", $expectativa."<br>", $urlFoto;
+echo "escolaridade".$escolaridade."<br>", "estuda".$estuda."<br>", "instituição".$instituicao."<br>", "curso".$curso."<br>", "motivo".$motivo."<br>", "razao".$razao."<br>", "exepriência".$experiencia."<br>", "expectativa".$expectativa."<br>", "urlFoto".$urlFoto;
 
 }else{
     return;}
